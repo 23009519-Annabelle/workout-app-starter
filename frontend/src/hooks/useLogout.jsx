d277a6662490ba96f831bba89baa4d714a733ee6
23009519-Annabelle <23009519@myrp.edu.sg>
2025-01-27 23:40:20 +0800

    C219_L11_WorkoutApp

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,42 +1,18 @@
-import { useState } from 'react';
-import { useAuthContext } from './useAuthContext';
+import { useAuthContext } from './useAuthContext'
+import { useWorkoutsContext } from './useWorkoutsContext'
 
-export const useLogin = () => {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const { dispatch } = useAuthContext();
+export const useLogout = () => {
+  const { dispatch } = useAuthContext()
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext()
 
-  const login = async (email, password) => {
-    setIsLoading(true);
-    setError(null);
+  const logout = () => {
+    // remove user from storage
+    localStorage.removeItem('user')
 
-    try {
-      const response = await fetch('/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
-      });
+    // dispatch logout action
+    dispatch({ type: 'LOGOUT' })
+    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
+  }
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const text = await response.text();
-      if (text) {
-        const data = JSON.parse(text);
-        // Save user to local storage
-        localStorage.setItem('user', JSON.stringify(data));
-        // Update auth context
-        dispatch({ type: 'LOGIN', payload: data });
-      } else {
-        throw new Error('Empty response');
-      }
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  return { login, error, isLoading };
-};
+  return { logout }
+}
