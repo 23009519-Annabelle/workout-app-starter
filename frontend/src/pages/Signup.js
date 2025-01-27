3bfa684fe34eac02ac624e202fcfcfa9d8178760
23009519-Annabelle <23009519@myrp.edu.sg>
2025-01-27 22:10:13 +0800

    C219L11WorkoutApp

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useSignup } from "..pages/useSignup"
+import { useSignup } from "../hooks/useSignup"
 
 const Signup = () => {
   const [email, setEmail] = useState('')
