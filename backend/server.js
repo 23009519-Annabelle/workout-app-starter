7ac5578fd35b7769080109a4b5abee2f9ed4f89b
23009519-Annabelle <23009519@myrp.edu.sg>
2025-01-28 09:21:19 +0800

    C219L11WorkoutApp

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+  origin: 'https://workout-app-starter-23009519-frontend.onrender.com', // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  credentials: true, // Enable credentials if using cookies or auth headers
+}));
+
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
